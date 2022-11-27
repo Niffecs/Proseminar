dce1cf302fa7a98e1165bc1b3d4b70859432459a
niffecs@gmail.com <niffecs@gmail.com>
2022-11-27 13:21:11 +0100

    Crypt File

--- upload.py
+++ upload.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python3
 
-import os 
+import os
 import ftplib
 import sys
+import hashlib
+import shutil
 
 # Load env
 FILE = sys.argv[1]
 HOST = os.environ["FTP_SERVER"]
 USER = os.environ["FTP_USER"]
-PWD  = os.environ["FTP_PWD"]
+PWD = os.environ["FTP_PWD"]
 
 
-# Test PDF 
-if not "pdf" in FILE: 
+# Test PDF
+if not "pdf" in FILE:
     print("no pdf")
     sys.exit(-1)
-    
+
+# Create Hash file
+hash = hashlib.md5(sys.argv[1][:-4].encode('utf-8')).hexdigest()
+shutil.copyfile(FILE, f"{hash}.pdf")
+FILE2 = f"{hash}.pdf"
+
+
 # FTP Gedoens
 ftp = ftplib.FTP(HOST, USER, PWD)
 ftp.encoding = "utf-8"
 with open(FILE, "rb") as file:
     ftp.storbinary(f"STOR {FILE}", file)
+ftp.cwd("loader")
+with open(FILE2, "rb") as file:
+    ftp.storbinary(f"STOR {FILE2}", file)
